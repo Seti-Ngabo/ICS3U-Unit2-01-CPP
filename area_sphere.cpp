28f7734676598d9d1b183a8820cd27760bebe8b4
Ubuntu <ubuntu@ip-172-31-51-232.ec2.internal>
2021-10-12 02:25:19 +0000

    added file

---
+++ area_sphere.cpp
@@ -0,0 +1,18 @@
+// Copyright (c) 2019 St. Mother Teresa HS All rights reserved.
+//
+// Created by: Seti Ngabo
+// Created on: Sept 2021
+// this program calculates the area and perimeter of a rectangle
+//   with dimensions 5cm x 3cm
+
+#include <iostream>
+#include <cmath>
+
+int main() {
+    std::cout << "If a circle has a radius of 15mm: " << std::endl;
+    std::cout << std::endl;
+    std::cout << "Area is " << (M_PI * pow(15, 2)) << "mm²." << std::endl;
+    std::cout << "Perimeter is " << (2 * M_PI * 15) << "mm." << std::endl;
+    std::cout << std::endl;
+    std::cout << "Done." << std::endl;
+}
